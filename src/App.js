13dca0cbdc3abcfe3d6ef04dc5fb750868a0ea58
whiskacz <pawelblasiak1985@gmail.com>
2023-03-12 21:05:03 +0100

    React formular with validation

--- src/App.js
+++ src/App.js
@@ -1,25 +1,53 @@
-import logo from './logo.svg';
+import React, {Component} from 'react'
 import './App.css';
 
-function App() {
+
+class App extends Component{
+  state = {
+    username : "",
+    email : "",
+    pass : "",
+    accept : false,
+  }
+  handleChange = (e) =>{
+    const name = e.target.name
+    const type = e.target.type
+    if( type === "text" || type === "password" || type === "email"){
+    const value = e.target.value
+    this.setState({
+      [name] : value
+    })}
+  else if(type === "checkbox"){
+    const checked = e.target.checked
+    this.setState({
+      [name] : checked
+  })
+  }
+}
+  handleSubmit = (e) =>{
+    e.preventDefault()
+  }
+
+  render(){
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <form onSubmit={this.handleSubmit}>
+        <label htmlFor="user"> Twoje imię: 
+        <input type="text" id='user' name='username' value={this.state.username} onChange={this.handleChange}/>
+        </label>
+        <label htmlFor="email"> Twój adres e-mail: 
+        <input type="email" id='user' name='email' value={this.state.email} onChange={this.handleChange}/>
+        </label>
+        <label htmlFor="password"> Podaj hasło: 
+        <input type="password" id='password' name='pass' value={this.state.pass} onChange={this.handleChange}/>
+        </label>
+        <label htmlFor="accept">
+        <input type="checkbox" id="accept" name='accept' checked={this.state.accept} onChange={this.handleChange}/>Wyrażam zgodę wszelaką
+        </label>
+        <button>Zapisz się</button>
+      </form>
     </div>
   );
 }
-
+}
 export default App;
