81ccc2748bd11220837e74f1b5fd409413dad333
whiskacz <pawelblasiak1985@gmail.com>
2023-03-13 17:31:23 +0100

    React formulat with validation

--- src/App.js
+++ src/App.js
@@ -8,6 +8,20 @@ class App extends Component{
     email : "",
     pass : "",
     accept : false,
+
+    errors: {
+      username : false,
+      email : false,
+      pass : false,
+      accept : false,
+    }
+    
+  }
+  messages = {
+    username_incorrect: "Nazwa musi być dłuższa niż 10 znaków i nie może zawierać spacji",
+    email_incorrect: "Brak @ w e-mail",
+    password_incorrect: "Hasło musi mieć 8 znaków",
+    accept_incorrect: "Niepotwierdzona zgoda"
   }
   handleChange = (e) =>{
     const name = e.target.name
@@ -26,26 +40,100 @@ class App extends Component{
 }
   handleSubmit = (e) =>{
     e.preventDefault()
+    const validation = this.formValidation()
+    console.log(validation)
+    if(validation.correct){
+      this.setState({
+        username : "",
+        email : "",
+        pass : "",
+        accept : false,
+        message: "Formularz został wysłany",
+    
+        errors: {
+          username : false,
+          email : false,
+          pass : false,
+          accept : false,
+        }
+      })
+      console.log("formularz wysłany")
+    } else{
+      this.setState({
+      errors: {
+        username : !validation.username,
+        email : !validation.email,
+        pass : !validation.password,
+        accept : !validation.accept,
+      }
+      })
+    
   }
+  }
+  formValidation = () =>{
+    
+    let username = false;
+    let email = false;
+    let accept =false;
+    let password = false;
+    let correct = false;
+    
+    if(this.state.username.length > 10 && this.state.username.indexOf(" ") === -1){
+      username = true;
+    }
+    if(this.state.email.indexOf("@") !== -1){
+      email = true;
+    }
+    if(this.state.pass.length === 8){
+      password = true;
+    }
+    if(this.state.accept){
+      accept = true;
+    }
+    if(username && password && email && accept){
+      correct = true
+    }
+    return({
+      correct,
+      username,
+      password,
+      email,
+      accept
+    })
+  
+  }
+  componentDidUpdate(){
+    if(this.state.message !== ""){
+      setTimeout(() => this.setState({
+        message: ""
+      }), 3000)
+    }
+  }
+
 
   render(){
   return (
     <div className="App">
-      <form onSubmit={this.handleSubmit}>
+      <form onSubmit={this.handleSubmit} noValidate>
         <label htmlFor="user"> Twoje imię: 
         <input type="text" id='user' name='username' value={this.state.username} onChange={this.handleChange}/>
+        {this.state.errors.username &&<span>{this.messages.username_incorrect}</span>}
         </label>
         <label htmlFor="email"> Twój adres e-mail: 
-        <input type="email" id='user' name='email' value={this.state.email} onChange={this.handleChange}/>
+        <input type="email" id='email' name='email' value={this.state.email} onChange={this.handleChange}/>
+        {this.state.errors.email &&<span>{this.messages.email_incorrect}</span>}
         </label>
         <label htmlFor="password"> Podaj hasło: 
         <input type="password" id='password' name='pass' value={this.state.pass} onChange={this.handleChange}/>
+        {this.state.errors.pass &&<span>{this.messages.password_incorrect}</span>}
         </label>
         <label htmlFor="accept">
         <input type="checkbox" id="accept" name='accept' checked={this.state.accept} onChange={this.handleChange}/>Wyrażam zgodę wszelaką
         </label>
+        {this.state.errors.accept &&<span>{this.messages.accept_incorrect}</span>}
         <button>Zapisz się</button>
       </form>
+      {this.state.message && <h3>{this.state.message}</h3>}
     </div>
   );
 }
